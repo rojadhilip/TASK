5997a8e99629ce891e4321bb5ffee9d79d0ce7cf
rojadhilip <164132300+rojadhilip@users.noreply.github.com>
2024-04-07 14:59:59 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Main {
+    public static void main(String[] args) {
+        int number =0;
+    //checks if number is greater than 0
+    if(number>0){
+        System.out.println("The number is positive.");
+
+    }   
+    //check if number is less dhan 0
+    else if (number<0){
+        System.out.println(" The number is negative.");
+    } 
+    //if both condition is false
+    else{
+        System.out.println("The number is 0.");
+    }
+    }
+    
+}
